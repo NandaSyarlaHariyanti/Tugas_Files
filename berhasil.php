50671ad458060a114d1c41dc113be6aeffe94a84
unknown <nandasyarla58911@gmail.com>
2023-05-25 17:47:48 +0700

    Halaman berhasil insert

---
+++ berhasil.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+    .my-button {
+        background-color: #4CAF50;
+        border: none;
+        color: black; /* Warna tulisan menjadi hitam */
+        padding: 10px 20px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        font-size: 16px;
+        margin: 4px 2px;
+        cursor: pointer;
+        border-radius: 4px;
+    }
+    .orange-button {
+        background-color: orange;
+    }
+
+    .yellow-button {
+        background-color: yellow;
+    }
+
+    </style>
+
+
+</head>
+<body>
+    <h2>DATA BERHASIL DITAMBAHKAN!</h2> 
+    <a href="insert.php" class="my-button yellow-button">Tambah Data  Buku</a>
+    <a href="read.php" class="my-button orange-button">Lihat Data Buku</a>
+
+    
+</body>
+</html>
